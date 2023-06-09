c72b84b17aff38e82981abdb62cbee74a7aa7e0e
shashikant <shashikant.thoghtwin@gmail.com>
2023-06-09 11:49:00 +0530

    fix(scope): refector code

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
 import { resolvers, typedefs } from './graphql';
 import { connection } from './config/';
-import cors from 'cors';
 import http from 'http';
 const app = express();
 const httpServer = http.createServer(app);
@@ -12,20 +10,22 @@ const expressServer = async () => {
     const server = new ApolloServer({
         typeDefs: typedefs,
         resolvers,
+        context: async ({ req }) => ({ token: req.headers.token }),
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
     await server.start();
-    app.use(
-        '/',
-        cors<cors.CorsRequest>(),
-        express.json(),
-        expressMiddleware(server)
-    );
+    server.applyMiddleware({ app });
     connection();
-
     await new Promise<void>((resolve) =>
         httpServer.listen({ port: 9000 }, resolve)
     );
-    console.log('🚀 Server ready at http://localhost:9000');
+    process.stdout.on('error', function (err) {
+        if (err.code == 'EPIPE') {
+            process.exit(0);
+        }
+    });
+    console.log(
+        `🚀 Server ready at http://localhost:9000${server.graphqlPath}`
+    );
 };
 expressServer();
