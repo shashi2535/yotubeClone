64baadb323193ad111a1fc7157aea4f648123e68
shashikant <shashikant.thoghtwin@gmail.com>
2023-08-04 16:41:12 +0530

    fix(scope): add if db not exist it created

--- src/config/data.ts
+++ src/config/data.ts
@@ -1,6 +1,7 @@
 import { Dialect, Sequelize } from 'sequelize';
 import { logger } from './logger';
 import { config } from './credential.env';
+import { createConnection } from 'mysql2';
 const {
   DB: { DB_DATABASE, DB_DIALECT, DB_HOST, DB_PASSWORD, DB_USERNAME },
 } = config;
@@ -10,6 +11,12 @@ const dbHost = DB_HOST;
 const dbDriver = DB_DIALECT as Dialect;
 const dbPassword = DB_PASSWORD;
 
+// Open the connection to MySQL server
+const mySqlConnection = createConnection({
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+});
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: dbDriver,
@@ -22,8 +29,9 @@ const connection = async () => {
     .then(() => {
       logger.info('😀 database connected successfully');
     })
-    .catch(() => {
+    .catch(async () => {
       logger.error('database not connected');
+      await mySqlConnection.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`);
     });
 };
 
